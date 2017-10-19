6c2b0f6e2970101bc57d1b1b73fdc0394f2e671e
maldicionchina <alexisrodgtz@gmail.com>
2017-10-19 16:51:21 -0500

    Hierarchy in progress...

---
+++ README.md
@@ -0,0 +1,8 @@
+# BlindTex
+
+El objetivo del proyecto BlindTeX es mejorar la accesibilidad de los do-
+cumentos matemáticos escritos en (La)TeX , para esto se intenta mejorar la
+
+
+capacidad del documento de transmitir la información matemática plasmada
+hacia el usuario por medio de pistas auditivas o textuales.
---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(name="BlindTex",
+      description="LaTeX ecuation processing",
+      version="0.1",
+      author="BlindTex Team",
+      author_email="",
+      #url="",
+      packages = [
+      ],
+      package_data = {
+       
+      },
+      scripts=['BlindText/blindtex'],
+)
