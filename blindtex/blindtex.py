0f8180bc36411a4fec706f87d39345b2064962db
Alexis Enrique Rodríguez Gutiérrez <alexisrodgtz@gmail.com>
2017-10-30 23:29:10 -0500

    Minor Fix, Creating some unit test.

--- blindtex/blindtex.py
+++ blindtex/blindtex.py
@@ -1,12 +1,12 @@
 
-from IO import *
+from io import io
 from sys import argv
 #The execution of the program.
 if(len(argv)==2):
-	IO.convertToHtml1(argv[1])
+	io.convertToHtml1(argv[1])
 	
 elif(len(argv)==3):
-	IO.convertToHtml2(argv[1],argv[2])
+	io.convertToHtml2(argv[1],argv[2])
 	
 else:
 	print"Wrong number of arguments, expected one or two (file name or filename + bibliography) and received %d."%(len(argv) -1)
--- tests/io/test_file.py
+++ tests/io/test_file.py
@@ -1,6 +1,12 @@
 import pytest
 from blindtex.io import file
 
+def test_extractContent():
+	contentFile = str(r'\begin{document} \title{Introduction to \LaTeX{}} \begin{abstract}\ The abstract text goes here. \end{abstract} \end{document}')
+
+	array = file.extractContent(contentFile)
+	assert array[1] == contentFile
+
 def test_read():
     contentFile = file.read("/path/to/dummy")
     assert "\{begin}..." == contentFile
