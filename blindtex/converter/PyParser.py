ea12a7d2ffe770698211c0cd256b49b7ad60d5a3
Jcmantillam <jcmantillam@unal.edu.co>
2017-11-09 07:20:48 -0500

    Implementado el módulo MathDictionary, corregido import de converter.py, añadida prueba de métodos de MathDictionary a testD

--- blindtex/converter/PyParser.py
+++ blindtex/converter/PyParser.py
@@ -2,28 +2,15 @@
 #Parser
 
 import ply.yacc as yacc
+from blindtex.converter.MathDictionary import *
 from PyLexer import tokens
-#Funciones: en esta sección, dejaremos todas las funciones que se requieran
-def addWord(key,value):
-    global Ordinary
-    if (Ordinary.get(key) is not None):
-        print('Key is alredy exist')
-    else:
-        Ordinary[str(key)] = value
-#--------------------------------------------------------------------------
+
 
 #TODO Que esto sea una estructura de datos, con funciones para cambiar la lectura(value) con mayor facilidad.
-Ordinary = {'alpha': 'alfa', 'beta': 'beta', 'gamma' : 'gamma', 'delta' : 'delta' ,'epsilon' : 'epsilon', 'varepsilon':'var epsilon' , 'zeta' : 'zeta', 'eta':'eta', 'theta' : 'teta','vartheta':'var teta', 'iota' : 'iota', 'kappa':'kappa', 'lambda':'lambda', 'mu':'mi', 'nu':'ni', 'xi':'xi', 'pi':'pi', 'varpi': 'var pi', 'rho':'ro', 'varrho': 'var ro','sigma':'sigma', 'varsigma': 'var sigma', 'tau':'tau', 'upsilon':'ipsilon', 'phi':'fi', 'varphi':'var fi', 'chi':'ji', 'psi':'psi', 'omega':'omega', 'Gamma': 'gama may&uacute;scula', 'Delta':'delta may&uacute;scula', 'Theta': 'teta may&uacute;scula', 'Lambda': 'lambda may&uacute;scula', 'Xi': 'xi may&uacute;scula', 'Pi': 'pi may&uacute;scula', 'Sigma': 'sigma may&uacute;scula', 'Upsilon': 'ipsilon may&uacute;scula', 'Phi': 'fi may&uacute;scula', 'Psi': 'psi may&uacute;scula', 'Omega': 'omega may&uacute;scula', 
-			'aleph': 'alef', 'hbar': 'hache barra', 'imath': 'i caligr&aacute;fica, sin punto', 'jmath': 'j caligr&aacute;fica, sin punto', 'ell' : 'ele caligr&aacute;fica','vp': 'p caligr&aacute;fica', 'Re': 'parte real','Im': 'parte imaginaria', 'partial': 'parcial', 'infty': 'infinito','prime': 'prima','emptyset':'conjunto vac&iacute;o','nabla':'nabla','surd':'ra&iacute;z','top': 'transpuesto', 'bot': 'perpendicular','|': 'paralelo, norma', 'angle': '&aacute;ngulo', 'triangle': 'tri&aacute;ngulo','backslash': 'barra invertida', 'forall':'para todo','exists':'existe','neg': 'negaci&oacute;n', 'flat': 'bemol', 'natural':'becuadro','sharp':'sostenido','clubsuit':'trebol','diamondsuit': 'diamante','heartsuit': 'corazón','spadsuit': 'picas'}
+
 
 #TODO: Acordar los nombres de algunos operadores.
 LargeOperators ={'sum': 'suma','prod':'producto', 'coprod':'coproducto','int': 'integral', 'oint': 'integral de contorno', 'bigcap': 'intersecci&oacute;n','bigcup': 'uni&oacute;n', 'bigsqcup':'Uni&oacute;n rect&aacute;ngular', 'bigvee' :'disyunci&oacute;n','bigwedge' : 'conjunci&oacute;n', 'bigodot': 'circumpunto','bigotimes': 'prducto tensorial','bigoplus': 'circumsuma', 'biguplus': 'uni&oacute;n con suma'}
-
-#Función para agregar al diccionario elementos
-key = ''
-value = ''#Estas variables de entrada se reconocerán posteriormente las dejo así por el momento, para probar con la GUI
-#addWord(key,value) Descomentar la línea cuando la función vaya a ser utulizada
-
 
 precedence = (	
 	('left','SUP','SUB', 'FRAC','ROOT'),
--- blindtex/converter/testD.py
+++ blindtex/converter/testD.py
@@ -1,13 +1,70 @@
-def addWord(key,value):
+#Funciones: en esta sección, dejaremos todas las funciones que se requieran
+
+#TODO Agrega un comando con su respectiva lectura
+def addReading(key,value):
+    str(value)
+    str(key)
     global Ordinary
     if (Ordinary.get(key) is not None):
         print('Key is alredy exist')
     else:
         Ordinary[str(key)] = value
+#--------------------------------------------------------------------------
+
+def changeReading(key,value):
+    str(value)
+    str(key)
+    global Ordinary
+    if (Ordinary.get(key) is None):
+        print('Key is alredy exist, do you want add it?')
+        An=input("y/n")
+        if An=='y':
+            addReading(key,value)
+        elif An=='n':
+            return
+    else:
+        Ordinary[str(key)] = value
+#--------------------------------------------------------------------------
+
+def showReading(key):
+    str(key)
+    global Ordinary
+    if (Ordinary.get(key) is None):
+        print('Key does not alredy exist')
+    else:
+        print(Ordinary.get(key))
+#--------------------------------------------------------------------------
+
+def showLaTex(key):
+    str(key)
+    if (Ordinary.get(key) is None):
+        print('Key does not alredy exist')
+    else:
+        print(key)
+#--------------------------------------------------------------------------
+
+def isThere(key):
+    if (Ordinary.get(key) is not None and Ordinary.get(key) is not None):
+        print(True)
+    else:
+        print(False)
 
 
 key = 'delta'
 value = 'delta'
 Ordinary = {'alpha': 'alfa', 'beta': 'beta', 'gamma' : 'gamma'}
-addWord(key,value)
+addReading(key,value)
+
+print(Ordinary)
+
+changeReading("Theta","3")
+
+print(Ordinary)
+
+showReading("Theta")
+
+isThere("Theta")
+
+
+
 
