e2755d5e7460f93de01c1a57d8e8741622afac60
Jcmantillam <jcmantillam@unal.edu.co>
2017-11-11 10:55:54 -0500

    Modificación de diccionarios, valores como lista, prueba de acceso a los usuarios

--- blindtex/converter/MathDictionary.py
+++ blindtex/converter/MathDictionary.py
@@ -1,17 +1,17 @@
 #-*-:coding:utf-8-*-
 #Diccionario que contiene todoas las palabras ordinarias que se pueden utilizar
-Ordinary = {'alpha': 'alfa', 'beta': 'beta', 'gamma' : 'gamma', 'delta' : 'delta' ,'epsilon' : 'epsilon', 'varepsilon':'var epsilon' , 'zeta' : 'zeta',
-            'eta':'eta', 'theta' : 'teta','vartheta':'var teta', 'iota' : 'iota', 'kappa':'kappa', 'lambda':'lambda', 'mu':'mi', 'nu':'ni', 'xi':'xi',
-            'pi':'pi', 'varpi': 'var pi', 'rho':'ro', 'varrho': 'var ro','sigma':'sigma', 'varsigma': 'var sigma', 'tau':'tau', 'upsilon':'ipsilon',
-            'phi':'fi', 'varphi':'var fi', 'chi':'ji', 'psi':'psi', 'omega':'omega', 'Gamma': 'gama may&uacute;scula', 'Delta':'delta may&uacute;scula',
-            'Theta': 'teta may&uacute;scula', 'Lambda': 'lambda may&uacute;scula', 'Xi': 'xi may&uacute;scula', 'Pi': 'pi may&uacute;scula',
-            'Sigma': 'sigma may&uacute;scula', 'Upsilon': 'ipsilon may&uacute;scula', 'Phi': 'fi may&uacute;scula', 'Psi': 'psi may&uacute;scula',
-            'Omega': 'omega may&uacute;scula','aleph': 'alef', 'hbar': 'hache barra', 'imath': 'i caligr&aacute;fica, sin punto',
-            'jmath': 'j caligr&aacute;fica, sin punto', 'ell' : 'ele caligr&aacute;fica','vp': 'p caligr&aacute;fica', 'Re': 'parte real',
-            'Im': 'parte imaginaria', 'partial': 'parcial', 'infty': 'infinito','prime': 'prima','emptyset':'conjunto vac&iacute;o','nabla':'nabla',
-            'surd':'ra&iacute;z','top': 'transpuesto', 'bot': 'perpendicular','|': 'paralelo, norma', 'angle': '&aacute;ngulo',
-            'triangle': 'tri&aacute;ngulo','backslash': 'barra invertida', 'forall':'para todo','exists':'existe','neg': 'negaci&oacute;n',
-            'flat': 'bemol', 'natural':'becuadro','sharp':'sostenido','clubsuit':'trebol','diamondsuit': 'diamante','heartsuit': 'corazón','spadsuit': 'picas'}
+Ordinary = {'alpha': ['alfa'], 'beta': ['beta'], 'gamma' : ['gamma'], 'delta' : ['delta'] ,'epsilon' : ['epsilon'], 'varepsilon':['var epsilon'] , 'zeta' : ['zeta'],
+            'eta':['eta'], 'theta' : ['teta'],'vartheta':['var teta'], 'iota' : ['iota'], 'kappa':['kappa'], 'lambda':['lambda'], 'mu':['mi'], 'nu':['ni'], 'xi':['xi'],
+            'pi':['pi'], 'varpi': ['var pi'], 'rho':['ro'], 'varrho': ['var ro'],'sigma':['sigma'], 'varsigma': ['var sigma'], 'tau':['tau'], 'upsilon':['ipsilon'],
+            'phi':['fi'], 'varphi':['var fi'], 'chi':['ji'], 'psi':['psi'], 'omega':['omega'], 'Gamma': ['gama may&uacute;scula'], 'Delta':['delta may&uacute;scula'],
+            'Theta': ['teta may&uacute;scula'], 'Lambda': ['lambda may&uacute;scula'], 'Xi': ['xi may&uacute;scula'], 'Pi': ['pi may&uacute;scula'],
+            'Sigma': ['sigma may&uacute;scula'], 'Upsilon': ['ipsilon may&uacute;scula'], 'Phi': ['fi may&uacute;scula'], 'Psi': ['psi may&uacute;scula'],
+            'Omega': ['omega may&uacute;scula'],'aleph': ['alef'], 'hbar': ['hache barra'], 'imath': ['i caligr&aacute;fica, sin punto'],
+            'jmath': ['j caligr&aacute;fica, sin punto'], 'ell' : ['ele caligr&aacute;fica'],'vp': ['p caligr&aacute;fica'], 'Re': ['parte real'],
+            'Im': ['parte imaginaria'], 'partial': ['parcial'], 'infty': ['infinito'],'prime': ['prima'],'emptyset':['conjunto vac&iacute;o'],'nabla':['nabla'],
+            'surd':['ra&iacute;z'],'top': ['transpuesto'], 'bot': ['perpendicular'],'|': ['paralelo, norma'], 'angle': ['&aacute;ngulo'],
+            'triangle': ['tri&aacute;ngulo'],'backslash': ['barra invertida'], 'forall':['para todo'],'exists':['existe'],'neg': ['negaci&oacute;n'],
+            'flat': ['bemol'], 'natural':['becuadro'],'sharp':['sostenido'],'clubsuit':['trebol'],'diamondsuit': ['diamante'],'heartsuit': ['corazón'],'spadsuit': ['picas']}
 
 
 #Funciones: en esta sección, dejaremos todas las funciones que se requieran
--- blindtex/converter/testD.py
+++ blindtex/converter/testD.py
@@ -9,62 +9,19 @@ def addReading(key,value):
         print('Key is alredy exist')
     else:
         Ordinary[str(key)] = value
-#--------------------------------------------------------------------------
-
-def changeReading(key,value):
-    str(value)
-    str(key)
-    global Ordinary
-    if (Ordinary.get(key) is None):
-        print('Key is alredy exist, do you want add it?')
-        An=input("y/n")
-        if An=='y':
-            addReading(key,value)
-        elif An=='n':
-            return
-    else:
-        Ordinary[str(key)] = value
-#--------------------------------------------------------------------------
-
-def showReading(key):
-    str(key)
-    global Ordinary
-    if (Ordinary.get(key) is None):
-        print('Key does not alredy exist')
-    else:
-        print(Ordinary.get(key))
-#--------------------------------------------------------------------------
-
-def showLaTex(key):
-    str(key)
-    if (Ordinary.get(key) is None):
-        print('Key does not alredy exist')
-    else:
-        print(key)
-#--------------------------------------------------------------------------
-
-def isThere(key):
-    if (Ordinary.get(key) is not None and Ordinary.get(key) is not None):
-        print(True)
-    else:
-        print(False)
-
 
 key = 'delta'
 value = 'delta'
-Ordinary = {'alpha': 'alfa', 'beta': 'beta', 'gamma' : 'gamma'}
-addReading(key,value)
+Ordinary = {'alpha': ['alfa', 'test @'], 'beta': ['beta'], 'gamma' : ['gamma']}
+
+
+
+Ordinary.values()
+
+for key in Ordinary.keys():
+    Ordinary.get(key).append('teta')
+    lista = Ordinary.get(key)
+    print(lista)
 
 print(Ordinary)
-
-changeReading("Theta","3")
-
-print(Ordinary)
-
-showReading("Theta")
-
-isThere("Theta")
-
-
-
 
