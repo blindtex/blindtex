42dfd0e8a3f79a69ec4e4345b41b08e097edeadb
David-Casas <dafecato4@gmail.com>
2017-11-24 11:56:08 -0500

    Regexes para tokenizar en un json y como cargarlas.

--- blindtex/converter/lexer.py
+++ blindtex/converter/lexer.py
@@ -1,11 +1,24 @@
 #Lexer of LaTeX math content
 
 import ply.lex as lex
+from ply.lex import TOKEN
+import json
 import re
 
+
+fileName = 'regexes.json'
+ 
 tokens = ('CHAR', 'SUP', 'SUB','BEGINBLOCK','ENDBLOCK', 'ORD', 'FRAC', 'ROOT', 'LARGEOP', 'BINOP','KBINOP','KBINREL', 'BINREL', 'NOT', 'FUNC', 'ARROW', 'KDELIMITER', 'DELIMITER', 'ACCENT','STYLE','DOTS','LIM', 'UNKNOWN')
 
 states = (('command', 'exclusive'),)
+
+try:
+	myFile = open(fileName, 'r')
+	dictOfDicts = json.load(myFile)
+	myFile.close()
+except IOError:
+	print 'File %s was not found.'%fileName
+
 
 def t_BEGINBLOCK(t):
 	r'\{'
@@ -26,15 +39,17 @@ def t_SUB(t):
 def t_COMMAND(t):
 	r'\\'
 	t.lexer.begin('command')
-	pass
+	return
 
+@TOKEN(dictOfDicts['largeOperator'])
 def t_command_LARGEOP(t):
-	r'sum|prod|coprod|int|oint|bigcap|bigcup|bigsqcup|bigvee|bigwedge|bigodot|bigotimes|bigoplus|biguplus'
+	
 	t.lexer.begin('INITIAL')
 	return t
 
+@TOKEN(dictOfDicts['ordinary'])
 def t_command_ORD(t):
-	r'(alpha)|(beta)|gamma|delta|epsilon|varepsilon|zeta|eta|theta|vartheta|iota|kappa|lambda|mu|nu|xi|pi|varpi|rho|varrho|sigma|varsigma|tau|upsilon|phi|varphi|chi|psi|omega|Gamma|Delta|Theta|Lambda|Xi|Pi|Sigma|Upsilon|Phi|Psi|Omega|aleph|hbar|imath|jmath|ell|vp|Re|Im|partial|infty|prime|emptyset|nabla|surd|top|bot|\||angle|triangle|backslash|forall|exists|neg|flat|natural|sharp|clubsuit|diamondsuit|heartsuit|spadsuit|lnot'
+	
 	t.lexer.begin('INITIAL')
 	return t
 
@@ -48,8 +63,9 @@ def t_command_ROOT(t):
 	t.lexer.begin('INITIAL')
 	return t
 
+@TOKEN(dictOfDicts['arrow'])
 def t_command_ARROW(t):
-	r'leftarrow|Leftarrow|rightarrow|Rightarrow|leftrightarrow|Leftrightarrow|mapsto|hookleftarrow|leftharpoonup|leftharpoondown|rightleftharpoons|longleftarrow|Longleftarrow|longrightarrow|Longrightarrow|longleftrightarrow|Longleftrightarrow|longmapsto|hookrightarrow|rightharpoonup|rightharpoondown|uparrow|Uparrow|downarrow|Downarrow|updownarrow|Updownarrow|nearrow|searrow|swarrow|nwarrow|to|gets|iff'
+	
 	t.lexer.begin('INITIAL')
 	return t
 
@@ -62,28 +78,33 @@ def t_KBINOP(t):#Binary operators that can be made from the keyboard.
 	r'\+|-|\*|/'
 	return t
 
+@TOKEN(dictOfDicts['dots'])
 def t_command_DOTS(t):
-	r'dots|ldots|cdots|vdots|ddots'
-	t.lexer.begin('INITIAL')
-	return t
 	
-def t_command_BINOP(t):
-	r'pm|mp|setminus|cdot|times|ast|star|diamond|circ|bullet|div|cap|cup|uplus|sqcap|sqcup|triangleleft|triangleright|wr|bigcirc|bigtriangleup|bigtriangledown|vee|wedge|oplus|ominus|otimes|oslash|odot|dagger|ddagger|amalg|lor|land'
 	t.lexer.begin('INITIAL')
 	return t
+
+@TOKEN(dictOfDicts['binOp'])	
+def t_command_BINOP(t):
+	
+	t.lexer.begin('INITIAL')
+	return t
+
 
 def t_KBINREL(t):
 	r'[=<>]'	
 	t.lexer.begin('INITIAL')
 	return t
 
+@TOKEN(dictOfDicts['binRel'])
 def t_command_BINREL(t):
-	r'leq|geq|equiv|prec|succ|sim|preceq|succeq|simeq|ll|gg|asymp|subset|supset|approx|subseteq|supseteq|cong|sqsubseteq|sqsupseteq|bowtie|in|ni|propto|vdash|dashv|models|smile|mid|doteq|frown|parallel|perp|neq|notin|ne|(le)|ge|owns'	
+		
 	t.lexer.begin('INITIAL')
 	return t
 
+@TOKEN(dictOfDicts['func'])
 def t_command_FUNC(t):
-	r'arccos|arcsin|arctan|arg|cos|cosh|cot|coth|csc|deg|det|dim|exp|gcd|hom|inf|ker|lg|liminf|limsup|ln|log|max|min|Pr|sec|sin|sinh|sup|tan|tanh'
+	
 	t.lexer.begin('INITIAL')
 	return t
 
@@ -96,18 +117,21 @@ def t_KDELIMITER(t):
 	r'\(|\)|\[|\]'
 	return t
 
+@TOKEN(dictOfDicts['delimiter'])
 def t_command_DELIMITER(t):
-	r'{|}|lfloor|rfloor|lceil|rceil|langle|rangle|backslash'
+	
 	t.lexer.begin('INITIAL')
 	return t
 
+@TOKEN(dictOfDicts['accent'])
 def t_command_ACCENT(t):
-	r'hat|check|breve|acute|grave|tilde|bar|vec|dot|ddot|widehat|widetilde'
+
 	t.lexer.begin('INITIAL')
 	return t
 
+@TOKEN(dictOfDicts['style'])
 def t_command_STYLE(t):
-	r'mathit|mathrm|mathbf|mathsf|mathtt|mathcal|boldmath'
+	
 	t.lexer.begin('INITIAL')
 	return t
 
