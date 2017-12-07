03f0e0a4488e994f1ce96a8b9690231cb88798c8
Jcmantillam <jcmantillam@unal.edu.co>
2017-12-06 19:12:15 -0500

    funcionalidad para identificar SO en el código, perimite ejecución en Windows y Linux.

--- blindtex/converter/converter.py
+++ blindtex/converter/converter.py
@@ -1,7 +1,21 @@
+import os
 from sys import argv
-from parser import convert
 
-print(convert(argv[1]))
+def run(arg):
+    try:
+        print(convert(arg))
+    except:
+        print("Error, please check the requirements and configurations")
+
+if os.name == "nt":
+    print os.name
+    from blindtex.converter.parser import convert
+    run(argv[1])
+if os.name == "posix":
+    print os.name
+    from parser import convert
+    run(argv[1])
+
 
 
 
